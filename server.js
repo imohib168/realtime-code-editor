fbdf35d82b2e22743b92680e6ce3a5168ac921d3
imohib168 <imohib168@gmail.com>
2022-03-25 17:55:35 +0500

    minor changes | deploting to prod

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
-// const ACTIONS = require('./ACTIONS');
 
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static('build'));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
